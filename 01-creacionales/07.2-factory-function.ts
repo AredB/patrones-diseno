6a3eeec885e2871ef8ea22d8195717ffd6d3a302
Ared <aredbruutal@gmail.com>
2026-04-20 23:00:35 +0200

    Ejercicio factory function

--- 01-creacionales/07.2-factory-function.ts
+++ 01-creacionales/07.2-factory-function.ts
@@ -31,9 +31,16 @@ function formatDate(date: Date): string {
 type LogLevel = 'info' | 'warn' | 'error';
 
 function createLogger(level: LogLevel) {
-  // Retorna una función que recibe el "message" como argumento
-  // Completar: implementar el logger con formato y color para cada nivel
-  throw new Error('Not implemented');
+  return function ( message: string ) {
+    const timestamp = formatDate(new Date());
+    const logColor = {
+        info: COLORS.white,
+        warn: COLORS.yellow,
+        error: COLORS.red,
+    };
+
+    return console.log(`%c[${ level.toUpperCase() }:${ timestamp }] ${ message }`, logColor[level]);
+  };
 }
 
 // Ejemplo de uso
